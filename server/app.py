ae97403f3de32408e712f169815f1f4f8b72a227
Celestine Mecheo <celestinenyamoita094@gmail.com>
2025-06-26 02:06:29 +0300

    Initial commit with Flask-RESTful CRUD setup

--- server/app.py
+++ server/app.py
@@ -16,72 +16,70 @@ db.init_app(app)
 
 api = Api(app)
 
-class Home(Resource):
-
+# Home route
+class Index(Resource):
     def get(self):
-        
         response_dict = {
-            "message": "Welcome to the Newsletter RESTful API",
+            "index": "Welcome to the Newsletter RESTful API",
         }
-        
-        response = make_response(
-            response_dict,
-            200,
-        )
+        return make_response(response_dict, 200)
 
-        return response
+api.add_resource(Index, '/')
 
-api.add_resource(Home, '/')
-
+# GET all newsletters and POST a new newsletter
 class Newsletters(Resource):
-
     def get(self):
-        
-        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
-
-        response = make_response(
-            response_dict_list,
-            200,
-        )
-
-        return response
+        newsletters = [n.to_dict() for n in Newsletter.query.all()]
+        return make_response(newsletters, 200)
 
     def post(self):
-        
-        new_record = Newsletter(
-            title=request.form['title'],
-            body=request.form['body'],
-        )
+        try:
+            new_record = Newsletter(
+                title=request.form['title'],
+                body=request.form['body'],
+            )
+            db.session.add(new_record)
+            db.session.commit()
 
-        db.session.add(new_record)
-        db.session.commit()
-
-        response_dict = new_record.to_dict()
-
-        response = make_response(
-            response_dict,
-            201,
-        )
-
-        return response
+            return make_response(new_record.to_dict(), 201)
+        except Exception as e:
+            return make_response({"error": str(e)}, 400)
 
 api.add_resource(Newsletters, '/newsletters')
 
+# GET, PATCH, DELETE by ID
 class NewsletterByID(Resource):
-
     def get(self, id):
+        record = Newsletter.query.filter_by(id=id).first()
+        if record:
+            return make_response(record.to_dict(), 200)
+        else:
+            return make_response({"error": "Newsletter not found"}, 404)
 
-        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+    def patch(self, id):
+        record = Newsletter.query.filter_by(id=id).first()
+        if not record:
+            return make_response({"error": "Newsletter not found"}, 404)
 
-        response = make_response(
-            response_dict,
-            200,
-        )
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
 
-        return response
+        db.session.add(record)
+        db.session.commit()
+
+        return make_response(record.to_dict(), 200)
+
+    def delete(self, id):
+        record = Newsletter.query.filter_by(id=id).first()
+        if not record:
+            return make_response({"error": "Newsletter not found"}, 404)
+
+        db.session.delete(record)
+        db.session.commit()
+
+        return make_response({"message": "record successfully deleted"}, 200)
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
